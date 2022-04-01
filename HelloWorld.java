54cf16b8adbdd217a912c885475c2b4435d15e0a
manubeco <emanuelbeco@outlook.com>
2022-04-01 20:26:48 -0300

    primeiro commit

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+import javax.lang.model.element.Element;
+
+public class HelloWorld {
+
+    public String mundo = "mundo";
+
+    public static void main(String[] args) {
+        public void sayHello(){
+            public String get(mundo);
+            System.out.println("Olá "+ Element.getAt);
+        }
+    }
+
+    public String getMundo() {
+        return mundo;
+    }
+
+    public void setMundo(String mundo) {
+        this.mundo = mundo;
+    }
+}
